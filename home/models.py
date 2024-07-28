922941a9315c1d9291e99e18b882db105da4bba7
Jeroen de Jong <jeroen.de.jong@online.de>
2024-07-28 16:52:52 +0200

    Introduce StreamField for body of normal page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,19 @@
-from django.db import models
+from wagtail.admin.panels.field_panel import FieldPanel
+from wagtail.blocks.field_block import RichTextBlock, CharBlock
+from wagtail.fields import StreamField
+from wagtail.images.blocks import ImageChooserBlock
 
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    body = StreamField([
+        ('heading', CharBlock(form_classname='title', label='Titel')),
+        ('subheading', CharBlock(form_classname='sub-title', label='Subtitel')),
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock())
+    ], default=[])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
