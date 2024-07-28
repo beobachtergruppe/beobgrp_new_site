6fe1044f89a9b49fc7b1f7c3f9257f15aa61f0a3
Jeroen de Jong <jeroen.de.jong@online.de>
2024-07-28 19:18:25 +0200

    Introduce model for events

--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
+from django.db.models import TextChoices
+from django.db.models.fields import DateTimeField, CharField, BooleanField
+from django.db.models.fields.files import ImageField
 from wagtail.admin.panels.field_panel import FieldPanel
 from wagtail.blocks.field_block import RichTextBlock, CharBlock
-from wagtail.fields import StreamField
+from wagtail.fields import StreamField, RichTextField
 from wagtail.images.blocks import ImageChooserBlock
 
 from wagtail.models import Page
@@ -17,3 +20,34 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body')
     ]
+
+class EventTypes(TextChoices):
+    TALK = "Vortrag"
+    HYBRID = "Hybride Vortrag"
+    ONLINE = "Online Vortrag"
+    OBSERVE = "Beobachtungsabend"
+    EXCURSION = "Ausflug"
+
+
+class SingleEvent(Page):
+    start_time = DateTimeField()
+    event_type = CharField(choices=EventTypes)
+    location = CharField(blank=True, max_length=120, default="Deutsches Museum")
+    referent = CharField(blank=True, max_length=120)
+    abstract = RichTextField(blank=True, max_length=800)
+    image = ImageField(blank=True)
+    cancelled = BooleanField(default=False)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('start_time'),
+        FieldPanel('event_type'),
+        FieldPanel('title'),
+        FieldPanel('location'),
+        FieldPanel('referent'),
+        FieldPanel('abstract'),
+        FieldPanel('image'),
+        FieldPanel('cancelled')
+    ]
+
+class EventPage(HomePage):
+    child_page_types = [SingleEvent]
