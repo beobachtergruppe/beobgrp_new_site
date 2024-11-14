88df34d81ea4302e7730803e07aaa39975455c33
Jeroen de Jong <jeroen.de.jong@online.de>
2024-11-15 00:23:21 +0100

    Improved models for events. New HTML5 template.

--- home/models.py
+++ home/models.py
@@ -2,7 +2,8 @@ from django.db.models import TextChoices
 from django.db.models.fields import DateTimeField, CharField, BooleanField
 from django.db.models.fields.files import ImageField
 from wagtail.admin.panels.field_panel import FieldPanel
-from wagtail.blocks.field_block import RichTextBlock, CharBlock
+from wagtail.blocks.field_block import RichTextBlock, CharBlock, ChoiceBlock
+from wagtail.blocks.struct_block import StructBlock
 from wagtail.fields import StreamField, RichTextField
 from wagtail.images.blocks import ImageChooserBlock
 
@@ -18,7 +19,7 @@ class HomePage(Page):
     ], default=[])
 
     content_panels = Page.content_panels + [
-        FieldPanel('body')
+        FieldPanel('body', heading='Inhalt')
     ]
 
 class EventTypes(TextChoices):
@@ -37,17 +38,36 @@ class SingleEvent(Page):
     abstract = RichTextField(blank=True, max_length=800)
     image = ImageField(blank=True)
     cancelled = BooleanField(default=False)
+    booked_out = BooleanField(default=False)
 
     content_panels = Page.content_panels + [
-        FieldPanel('start_time'),
-        FieldPanel('event_type'),
-        FieldPanel('title'),
-        FieldPanel('location'),
-        FieldPanel('referent'),
-        FieldPanel('abstract'),
-        FieldPanel('image'),
-        FieldPanel('cancelled')
+        FieldPanel('start_time', heading='Zeit'),
+        FieldPanel('event_type', heading='Art der Veranstaltung'),
+        FieldPanel('title', heading='Titel'),
+        FieldPanel('location', heading='Ort'),
+        FieldPanel('referent', heading='Referent'),
+        FieldPanel('abstract', heading='Zusammenfassung'),
+        FieldPanel('image', heading='Foto'),
+        FieldPanel('cancelled', heading='Abgesagt'),
+        FieldPanel('booked_out', heading='Ausgebucht')
     ]
 
-class EventPage(HomePage):
+
+class EventListBlock(StructBlock):
+    event_type = ChoiceBlock(choices=EventTypes.choices, required=True, label="Art der Veranstaltung",
+                             help_text="Liste von Veranstaltungen")
+
+class EventPage(Page):
+    body = StreamField([
+        ('heading', CharBlock(form_classname='title', label='Titel')),
+        ('subheading', CharBlock(form_classname='sub-title', label='Subtitel')),
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('event_list', EventListBlock())
+    ], default=[])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', heading='Inhalt')
+    ]
+
     child_page_types = [SingleEvent]
