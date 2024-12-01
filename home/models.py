63922c18a02e9fea7196bc34c91d49604c45b46a
Jeroen de Jong <jeroen.de.jong@online.de>
2024-12-01 17:08:20 +0100

    Improve CSS and add multiple headers to page models

--- home/models.py
+++ home/models.py
@@ -1,22 +1,27 @@
+from typing import Tuple
+
 from django.db.models import TextChoices
 from django.db.models.fields import DateTimeField, CharField, BooleanField
 from django.db.models.fields.files import ImageField
 from wagtail.admin.panels.field_panel import FieldPanel
-from wagtail.blocks.field_block import RichTextBlock, CharBlock, ChoiceBlock
+from wagtail.blocks.field_block import RichTextBlock, CharBlock, ChoiceBlock, FieldBlock
 from wagtail.blocks.struct_block import StructBlock
 from wagtail.fields import StreamField, RichTextField
 from wagtail.images.blocks import ImageChooserBlock
 
 from wagtail.models import Page
+from copy import deepcopy
 
+gen_body_content: list[Tuple[str, FieldBlock]] = [
+    ('h1', CharBlock(form_classname='h1', label='Kopfzeile 1')),
+    ('h2', CharBlock(form_classname='h2', label='Kopfzeile 2')),
+    ('h3', CharBlock(form_classname='h3', label='Kopfzeile 3')),
+    ('paragraph', RichTextBlock()),
+    ('image', ImageChooserBlock())
+]
 
 class HomePage(Page):
-    body = StreamField([
-        ('heading', CharBlock(form_classname='title', label='Titel')),
-        ('subheading', CharBlock(form_classname='sub-title', label='Subtitel')),
-        ('paragraph', RichTextBlock()),
-        ('image', ImageChooserBlock())
-    ], default=[])
+    body = StreamField(gen_body_content, default=[])
 
     content_panels = Page.content_panels + [
         FieldPanel('body', heading='Inhalt')
@@ -57,14 +62,11 @@ class EventListBlock(StructBlock):
     event_type = ChoiceBlock(choices=EventTypes.choices, required=True, label="Art der Veranstaltung",
                              help_text="Liste von Veranstaltungen")
 
+blocks_for_event_body = deepcopy(gen_body_content)
+blocks_for_event_body.append(('event_list', EventListBlock())) # type: ignore
+
 class EventPage(Page):
-    body = StreamField([
-        ('heading', CharBlock(form_classname='title', label='Titel')),
-        ('subheading', CharBlock(form_classname='sub-title', label='Subtitel')),
-        ('paragraph', RichTextBlock()),
-        ('image', ImageChooserBlock()),
-        ('event_list', EventListBlock())
-    ], default=[])
+    body = StreamField(blocks_for_event_body, default=[])
 
     content_panels = Page.content_panels + [
         FieldPanel('body', heading='Inhalt')
