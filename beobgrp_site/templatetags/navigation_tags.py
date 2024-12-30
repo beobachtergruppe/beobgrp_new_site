b1a886d70ccf174717eafaa4a3307cdbcb0e14fd
Jeroen de Jong <jeroen.de.jong@online.de>
2024-12-30 13:50:19 +0100

    Properly render the desktop menu horizontally.

--- beobgrp_site/templatetags/navigation_tags.py
+++ beobgrp_site/templatetags/navigation_tags.py
@@ -1,10 +1,21 @@
-from django import template
+import urllib
 
+from django import template
+from urllib.parse import urlparse
 # import site:
-from wagtail.models import Site
+from wagtail.models import Site, Page
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def get_site_root(context):
+def get_site_root(context: dict) -> Page:
     return Site.find_for_request(context["request"]).root_page
+
+@register.simple_tag(takes_context=True)
+def add_active_class(context: dict, menu_url: str)->str:
+    menu_path = urlparse(menu_url).path
+    current_path = context["request"].path
+    if menu_path == current_path:
+        return "active"
+    else:
+        return ""
