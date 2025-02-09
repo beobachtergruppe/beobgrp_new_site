5d7309875e9c90991f1324c2f0806213009b3cc2
Jeroen de Jong <jeroen.de.jong@online.de>
2025-02-09 18:53:15 +0100

    Ensure that /media is persistent and owned by wagtail. Introduce env variable to configure /media directory.

--- docker_start_server.sh
+++ docker_start_server.sh
@@ -7,13 +7,25 @@ if [ -z $SITE_INIT_MODE ]; then
   exit 1
 fi
 
+# change user and group id of mounted volume to match the docker user
+chown -R wagtail:wagtail /media
+
+# change user to wagtail
+su wagtail
+
+# commands to configure and start the server
+
+python manage.py collectstatic --noinput --clear
+
 case "$SITE_INIT_MODE" in
   migrate)
     python manage.py migrate --noinput
     ;;
   restore)
+    python manage.py migrate --noinput
     python manage.py dbrestore --noinput
     python manage.py mediarestore --noinput
+    python manage.py migrate --noinput
     ;;
   none)
     ;;
