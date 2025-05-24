b4c44b9daae23254b68ad623e45047a7edb48de3
Jeroen de Jong <jeroen.de.jong@online.de>
2025-05-24 22:58:40 +0200

    Added if--exists to restore options to prevent crash on cleaning non-existing tables

--- restore.sh
+++ restore.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
-python manage.py dbrestore --noinput
+python manage.py dbrestore --noinput --pg-options="--if-exists"
 python manage.py mediarestore --noinput
 python manage.py migrate --noinput
