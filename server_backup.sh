3ca68fc33dad5a71411ba10f421af086ef2b7d93
Jeroen de Jong <jeroen.de.jong@online.de>
2026-02-16 23:24:06 +0100

    Allow also backups from development server

--- server_backup.sh
+++ server_backup.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
 # Backups the databases and media and removes backups older than one week
-docker exec beobgrp_new_site-wagtail-1 ./backup.sh && \
+# Usage: ./server_backup.sh [--dev]
+# Use --dev to backup from the _dev pod instead of the default pod
+
+POD_NAME="beobgrp_new_site-wagtail-1"
+
+# Parse arguments
+for arg in "$@"; do
+  case $arg in
+    --dev)
+      POD_NAME="beobgrp_new_site-wagtail_dev-1"
+      shift
+      ;;
+  esac
+done
+
+docker exec "$POD_NAME" ./backup.sh && \
 test -n $DJANGO_BACKUP_DIR && find $DJANGO_BACKUP_DIR -maxdepth 1 -mtime +3 \( -name '*.tar' -or -name '*.bin' \) -print -delete
